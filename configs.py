a3d60aa946036b0a140c7b4733bf41cc9b9cb8bd
Giri Giri <128954293+girigiri2829@users.noreply.github.com>
2024-03-18 21:13:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -20,6 +20,7 @@ class Config(object):
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+Credit @Gi_Tamil_Cartoons
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
